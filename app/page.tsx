70572ed439480d150dd900c7c275636810a266bf
markohautala <markohautala@live.se>
2024-07-23 16:29:46 +0200

    Add v0 styling for the chat-component

--- app/page.tsx
+++ app/page.tsx
@@ -1,26 +1,7 @@
 'use client';
 
-import { useChat } from 'ai/react';
+import { Component } from '@/components/component/component';
 
 export default function Chat() {
-  const { messages, input, handleInputChange, handleSubmit } = useChat();
-  return (
-    <div className="flex flex-col w-full max-w-md py-24 mx-auto stretch">
-      {messages.map(m => (
-        <div key={m.id} className="whitespace-pre-wrap">
-          {m.role === 'user' ? 'User: ' : 'AI: '}
-          {m.content}
-        </div>
-      ))}
-
-      <form onSubmit={handleSubmit}>
-        <input
-          className="fixed bottom-0 w-full max-w-md p-2 mb-8 border border-gray-300 rounded shadow-xl text-black"
-          value={input}
-          placeholder="Say something..."
-          onChange={handleInputChange}
-        />
-      </form>
-    </div>
-  );
+  return <Component />;
 }
